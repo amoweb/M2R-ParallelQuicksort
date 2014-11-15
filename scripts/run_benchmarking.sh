462dba781a2d1c2675a7068899ccad1430d96c95
Amaury Graillat <amaury@amoweb.fr>
2014-11-15 12:28:01 +0000

    Mesurements

--- scripts/run_benchmarking.sh
+++ scripts/run_benchmarking.sh
@@ -4,8 +4,10 @@ mkdir -p $OUTPUT_DIRECTORY
 OUTPUT_FILE=$OUTPUT_DIRECTORY/measurements_`date +%R`.txt
 
 touch $OUTPUT_FILE
-for i in 100 1000 10000 100000 1000000 10000000; do
+for i in 100 1000 10000 100000 1000000 10000000 30000000 60000000 90000000; do
+        echo "Size: $i";
         for j in 0 1 2 3 4 5 6 7 8; do
+            echo "$j";
             for rep in `seq 1 5`; do
                 echo "Size: $i" >> $OUTPUT_FILE;
                 echo "Threads: $j" >> $OUTPUT_FILE;
